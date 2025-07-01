aaeee80fd8a92e476a5e36dd76ef3720d188a45f
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-07-01 17:39:34 -0300

    commit

--- yokebot.py
+++ yokebot.py
@@ -5,3 +5,21 @@ import os
 import discord
 
 YOKEBOT_TOKEN = os.getenv("DISCORD_TOKEN") or exit(1)
+
+intents = discord.Intents.default()
+
+yokebot = discord.Client(intents=intents)
+
+@yokebot.event
+async def on_ready():
+    print('logged as', yokebot.user)
+
+@yokebot.event
+async def on_message(message):
+    print('new message from', message.author, "on", message.channel)
+    if message.author != yokebot.user:
+        await message.channel.send(f'hey, {message.author}!')
+
+yokebot.run(token=YOKEBOT_TOKEN)
+
+
